1954faa4d486ca89750217831467e4893b90937e
seekhow <seekhowqiuhao@gmail.com>
2018-09-18 16:43:50 +0800

    fix: 修改lesson model

--- model/lesson.js
+++ model/lesson.js
@@ -7,13 +7,9 @@ module.exports= mongoose.model(
   'Lesson',
   new Schema({
     name: { type: String, default: '' }, // 课程名
-    chapter: new Schema({
-      name: { type: String, default: '' }, // 章节名
-      subChapter: new Schema({
-        name: { type: String, default: '' }, // 子章节名
-        video: { type: Schema.Types.ObjectId, ref: '' } // 每个子章节关联一个视频，通过ObjectId ref关联video实体
-      })
-    })
+    chapter: { type: String, default: '' }, // 章节名
+    subChapter: { type: String, default: '' }, // 子章节名
+    video: { type: Schema.Types.ObjectId, ref: '' } // 每个子章节关联一个视频，通过ObjectId ref关联video实体
   },{
     toObject: { transform },
     toJSON: { transform }
